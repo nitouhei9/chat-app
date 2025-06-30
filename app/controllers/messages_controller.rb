c3e7f55ba7a518c69f4ef24f96495078e93778a7
nitouhei9 <kawachan.genmame@gmail.com>
2025-06-30 18:22:48 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,6 +8,11 @@ class MessagesController < ApplicationController
     @room = Room.find(params[:room_id])
     @message = @room.messages.new(message_params)
     @message.save
+    if @message.save
+         redirect_to room_messages_path(@room)
+    else
+      render :index, status: :unprocessable_entity
+    end
   end
 
   private
